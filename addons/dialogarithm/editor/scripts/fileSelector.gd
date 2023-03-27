d1c9229aa2e6966f13f00d9959debc366f08e7c0
Samferos <68814507+Samferos@users.noreply.github.com>
2023-03-27 09:40:53 +0200

    new fixes for the plugin ayyyayyyy

--- addons/dialogarithm/editor/scripts/fileSelector.gd
+++ addons/dialogarithm/editor/scripts/fileSelector.gd
@@ -1,18 +1,19 @@
 @tool
-extends HBoxContainer
+extends Control
 
 var positionId = 1
 
 func _enter_tree():
-	$LeftPanel/Label.text = "Position " + str(positionId)
-	$LeftPanel/Button.connect("pressed", Callable(self, "SearchFile"))
+	$HBoxContainer/LeftPanel/Label.text = "Position " + str(positionId)
+	$HBoxContainer/LeftPanel/Button.connect("pressed", Callable(self, "SearchFile"))
 
 func SearchFile():
 	var window = FileDialog.new()
 	window.file_mode = FileDialog.FILE_MODE_OPEN_FILE
 	window.add_filter("*.png", "Transparent Images")
 	window.connect("file_selected", Callable(self, "UpdateSprite"))
+	window.visible = true
 	EditorPlugin.new().get_editor_interface().get_base_control().add_child(window)
 
 func UpdateSprite(sprite):
-	get_child(2).texture = sprite
+	get_child(2).texture = load(sprite)
--- addons/dialogarithm/editor/scripts/spriteSlotManager.gd
+++ addons/dialogarithm/editor/scripts/spriteSlotManager.gd
@@ -1,28 +1,44 @@
 @tool
 extends Control
 
-var slots = {position1 = false, position2 = false, position3 = false, position4 = false}
 const textureSlot = "res://addons/dialogarithm/editor/sprite_selector.tscn"
+@export var positionTypes := 5
+var slots = {"position1" = false, "position2" = false, "position3" = false, "position4" = false, "position5" = false}
 
 func _enter_tree():
+	#Slots Values Dictionnary Init
+	for i in positionTypes:
+		slots["position" + str(i + 1)] = false
+	for i in positionTypes:
+		if $VBoxContainer/Positions.get_child(i) is CheckButton:
+			#Slots Values Get from existing CheckButton
+			slots["position" + str(i + 1)] = $VBoxContainer/Positions.get_child(i).button_pressed
+		else:
+			var switch = CheckButton.new()
+			switch.text = "Position " + str(i + 1)
+			switch.button_pressed = slots["position" + str(i+1)]
+			$VBoxContainer/Positions.add_child(switch)
 	SetSlots()
+	print(slots)
 
 func SetSlots():
-	for i in get_child_count():
-		if i > 1:
-			get_child(i).queue_free()
+	for i in $VBoxContainer/PanelContainer/VBoxContainer/Panel/Slots.get_children():
+		if i is Label:
+			i.queue_free()
+		elif slots["position" + str(i.positionId)]:
+			i.queue_free()
 	if slots.find_key(true) == null:
 		var emptytext = Label.new()
 		emptytext.text = "brain empty no thoughts"
 		emptytext.add_theme_color_override("font_color", Color.DARK_GRAY)
-		add_child(emptytext)
+		$VBoxContainer/PanelContainer/VBoxContainer/Panel/Slots.add_child(emptytext)
 	else:
-		for i in range(4):
+		for i in range(positionTypes):
 			var slotname = "position" + str(i + 1)
 			if slots[slotname]:
 				var NewSlot = preload("res://addons/dialogarithm/editor/sprite_selector.tscn").instantiate()
 				NewSlot.positionId = i + 1
-				add_child(NewSlot)
+				$VBoxContainer/PanelContainer/VBoxContainer/Panel/Slots.add_child(NewSlot)
 
 func TogglePosition(state, id):
 	var slotname = "position" + str(id)
