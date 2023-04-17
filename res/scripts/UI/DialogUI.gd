e8a744add44649175db1e1cdd5b8bf37b4c285ec
Samuel <samuel.feros@hotmail.com>
2023-04-17 21:28:31 +0200

    fak i forgor

--- res/scripts/UI/DialogUI.gd
+++ res/scripts/UI/DialogUI.gd
@@ -1,9 +1,22 @@
 extends Control
 
+var step = 0
+var currentDialog
+
+func StartDialog(dialogFile : String):
+	var file = FileAccess.open(dialogFile, FileAccess.READ)
+	currentDialog = Dialog.new()
+	var data = JSON.parse_string(file.get_as_text())
+
+class Dialog:
+	var data
+	
+	func nextBlock():
+		pass
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
